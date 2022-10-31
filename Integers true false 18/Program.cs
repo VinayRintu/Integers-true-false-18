ec10e40c1b0d4cee5d404ed5ff1494a1843a456f
Rintu <Rintu@VINDUJA>
2022-10-31 16:46:25 +0530

    Add project files.

---
+++ Integers true false 18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integers_true_false_18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter first number");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("enter second number");
+            int y=Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine((x < 0 && y > 0)||(x > 0 && y < 0));
+            //Console.ReadLine();
+            
+            if((x < 0 && y > 0) || (x > 0 && y < 0))
+            {
+                Console.WriteLine("True");
+                Console.ReadLine();
+            }
+        }
+    }
+}
